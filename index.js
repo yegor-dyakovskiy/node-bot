396b37de032aa3b45d85645fafab45c618b127f2
Yagor <yegor.dyakovskiy@gmail.com>
2024-12-06 21:23:19 +0500

    add express lib for render

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import express from 'express';
 import bot from './bot.js';
 
-bot.launch().then(() => {
-    console.log('Бот успешно запущен!');
+const PORT = process.env.PORT || 3000; // Render указывает порт через переменную PORT
+const app = express();
+
+// Устанавливаем Webhook
+const RENDER_URL = 'https://node-bot-r093.onrender.com'; // Замените на ваш URL
+bot.telegram.setWebhook(`${RENDER_URL}/webhook`);
+
+// Маршрут для обработки Webhook
+app.use(bot.webhookCallback('/webhook'));
+
+// Запускаем сервер
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Webhook set to ${RENDER_URL}/webhook`);
 });
