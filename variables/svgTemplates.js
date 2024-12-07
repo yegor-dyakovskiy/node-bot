6bd15d9637e75a31090026014b70fd2636c48bb5
Yagor <yegor.dyakovskiy@gmail.com>
2024-12-07 20:20:27 +0500

    add montserrat

--- variables/svgTemplates.js
+++ variables/svgTemplates.js
@@ -1,84 +1,71 @@
-import fs from 'fs';
-import path from 'path';
-
-// Получаем путь к директории через import.meta.url
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-// Путь к шрифту
-const fontPath = path.join(__dirname, 'centurygothic.ttf');
-
-// Преобразуем шрифт в base64
-const fontBase64 = fs.readFileSync(fontPath, 'base64');
-
+// Шаблон для подключения шрифта Montserrat через Google Fonts
 const fontFaceStyle = `
   <style>
-    @font-face {
-      font-family: 'Century Gothic', sans-serif;
-      src: url('data:font/ttf;base64,${fontBase64}') format('truetype');
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+    text {
+      font-family: 'Montserrat', sans-serif;
     }
   </style>
 `;
 
-console.log(fontFaceStyle);
-
 // Шаблон градиента для сториса сверху
 export const createStoryUpGradientSvg = (fixedSize) => {
     return `
-  <svg width="${fixedSize}" height="${fixedSize + 300}">
-    <defs>
-      <linearGradient id="storyUpGradient" x1="100%" y1="0%" x2="40%" y2="0%">
-        <stop offset="0%" style="stop-color: rgba(25, 91, 108, 0); stop-opacity: 1" />
-        <stop offset="100%" style="stop-color: rgba(25, 91, 108, 1); stop-opacity: 1" />
-      </linearGradient>
-    </defs>
-    <rect x="0" y="0" width="100%" height="17%" fill="url(#storyUpGradient)" />
-  </svg>
-`;
+    <svg width="${fixedSize}" height="${fixedSize + 300}">
+      <defs>
+        <linearGradient id="storyUpGradient" x1="100%" y1="0%" x2="40%" y2="0%">
+          <stop offset="0%" style="stop-color: rgba(25, 91, 108, 0); stop-opacity: 1" />
+          <stop offset="100%" style="stop-color: rgba(25, 91, 108, 1); stop-opacity: 1" />
+        </linearGradient>
+      </defs>
+      <rect x="0" y="0" width="100%" height="17%" fill="url(#storyUpGradient)" />
+    </svg>
+  `;
 };
 
 // Шаблон текста для сториса сверху
 export const createStoryUpTextSvg = (fixedSize, firstText, secondText) => {
     return `
-  <svg width="${fixedSize}" height="${fixedSize + 300}">
-    ${fontFaceStyle}
-    <text x="50" y="${
-        fixedSize - 990
-    }" font-family="Century Gothic" font-size="70" fill="white" text-anchor="start">${firstText}</text>
-    <text x="50" y="${
-        fixedSize - 890
-    }" font-family="Century Gothic" font-size="70" fill="white" text-anchor="start">${secondText}</text>
-  </svg>
-`;
+    <svg width="${fixedSize}" height="${fixedSize + 300}">
+      ${fontFaceStyle}
+      <text x="50" y="${
+          fixedSize - 990
+      }" font-family="Montserrat" font-size="70" fill="white" text-anchor="start">${firstText}</text>
+      <text x="50" y="${
+          fixedSize - 890
+      }" font-family="Montserrat" font-size="70" fill="white" text-anchor="start">${secondText}</text>
+    </svg>
+  `;
 };
 
 // Шаблон градиента для сториса снизу
 export const createStoryDownGradientSvg = (fixedSize) => {
     return `
-<svg width="${fixedSize}" height="${fixedSize + 300}">
-  <defs>
-    <linearGradient id="storyUpGradient" x1="100%" y1="0%" x2="40%" y2="0%">
-      <stop offset="0%" style="stop-color: rgba(25, 91, 108, 0); stop-opacity: 1" />
-      <stop offset="100%" style="stop-color: rgba(25, 91, 108, 1); stop-opacity: 1" />
-    </linearGradient>
-  </defs>
-  <rect x="0" y="1150" width="100%" height="17%" fill="url(#storyUpGradient)" />
-</svg>
-`;
+    <svg width="${fixedSize}" height="${fixedSize + 300}">
+      <defs>
+        <linearGradient id="storyUpGradient" x1="100%" y1="0%" x2="40%" y2="0%">
+          <stop offset="0%" style="stop-color: rgba(25, 91, 108, 0); stop-opacity: 1" />
+          <stop offset="100%" style="stop-color: rgba(25, 91, 108, 1); stop-opacity: 1" />
+        </linearGradient>
+      </defs>
+      <rect x="0" y="1150" width="100%" height="17%" fill="url(#storyUpGradient)" />
+    </svg>
+  `;
 };
 
 // Шаблон текста для сториса снизу
 export const createStoryDownTextSvg = (fixedSize, firstText, secondText) => {
     return `
-<svg width="${fixedSize}" height="${fixedSize + 300}">
-  ${fontFaceStyle}
-  <text x="50" y="${
-      fixedSize + 160
-  }" font-family="Century Gothic" font-size="70" fill="white" text-anchor="start">${firstText}</text>
-  <text x="50" y="${
-      fixedSize + 260
-  }" font-family="Century Gothic" font-size="70" fill="white" text-anchor="start">${secondText}</text>
-</svg>
-`;
+    <svg width="${fixedSize}" height="${fixedSize + 300}">
+      ${fontFaceStyle}
+      <text x="50" y="${
+          fixedSize + 160
+      }" font-family="Montserrat" font-size="70" fill="white" text-anchor="start">${firstText}</text>
+      <text x="50" y="${
+          fixedSize + 260
+      }" font-family="Montserrat" font-size="70" fill="white" text-anchor="start">${secondText}</text>
+    </svg>
+  `;
 };
 
 // Шаблон градиента для поста
@@ -103,10 +90,10 @@ export const createTextSvg = (fixedSize, firstText, secondText) => {
       ${fontFaceStyle}
       <text x="50%" y="${
           fixedSize - 150
-      }" font-family="Century Gothic" font-size="75" fill="white" text-anchor="middle">${firstText}</text>
+      }" font-family="Montserrat" font-size="75" fill="white" text-anchor="middle">${firstText}</text>
       <text x="50%" y="${
           fixedSize - 40
-      }" font-family="Century Gothic" font-size="75" fill="white" text-anchor="middle">${secondText}</text>
+      }" font-family="Montserrat" font-size="75" fill="white" text-anchor="middle">${secondText}</text>
     </svg>
   `;
 };
